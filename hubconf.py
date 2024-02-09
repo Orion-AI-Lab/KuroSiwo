7cdc1be0ab4d1a2657f531ec89936dde21548627
Brandon Victor <multihuntr@hotmail.com>
2024-02-09 13:58:43 +0000

    Added dem processing to Wrapper. Made SNUNet use DEM.

--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,8 @@
-dependencies = ["einops"]
+dependencies = ["einops", "richdem"]
 
+import einops
+import richdem as rd
+import numpy as np
 import torch
 import torch.nn as nn
 import torchvision
@@ -12,7 +15,22 @@ from models.vision_transformer import ViT
 # Hard-coded because these should be decoupled from changing configs
 IMG_MEANS = [0.0953, 0.0264]
 IMG_STDS = [0.0427, 0.0215]
+DEM_MEAN = 67.0293
+DEM_STD = 1765.0062
 EPS = 1e-7
+
+
+def _calculate_slope(dem, nodata):
+    """
+    Calculates the slope of the DEM. DEM should be shaped [b h w]
+    """
+    res = torch.empty_like(dem)
+    dem_np = dem.numpy()
+    for i in range(dem.shape[0]):
+        dem_i = rd.rdarray(dem_np[i], no_data=nodata)
+        dem_i = rd.TerrainAttribute(dem_i, attrib="slope_riserun")
+        res[i] = torch.tensor(np.asarray(dem_i.data))
+    return res
 
 
 class PreprocessingWrapper(nn.Module):
@@ -25,18 +43,36 @@ class PreprocessingWrapper(nn.Module):
         [pre1] pre2 post
     """
 
-    def __init__(self, model, means, stds, append_ratio=False, cat_input=True):
+    def __init__(
+        self,
+        model,
+        means,
+        stds,
+        append_ratio=False,
+        cat_input=True,
+        requires_dem=False,
+        requires_slope=False,
+        dem_mean=None,
+        dem_std=None,
+    ):
         super().__init__()
         self.model = model
         self.means = means
         self.stds = stds
         self.append_ratio = append_ratio
         self.cat_input = cat_input
+        self.requires_dem = requires_dem
+        self.requires_slope = requires_slope
+        self.dem_mean = dem_mean
+        self.dem_std = dem_std
         self.transform = torchvision.transforms.Normalize(means, stds)
+        if (requires_dem or requires_slope) and (dem_mean is None or dem_std is None):
+            raise Exception("Using a DEM requires normalisation parameters.")
+        self.dem_transform = torchvision.transforms.Normalize(dem_mean, dem_std)
 
-    def forward(self, images, dem=None):
-        if dem is not None:
-            raise NotImplementedError("DEM not implemented for model wrapper")
+    def forward(self, images, dem=None, dem_nodata=np.nan):
+        if (self.requires_dem or self.requires_slope) and dem is None:
+            raise Exception("This model requires a DEM.")
 
         # Normalise images
         norm_images = [self.transform(image) for image in images]
@@ -44,6 +80,13 @@ class PreprocessingWrapper(nn.Module):
         if self.append_ratio:
             # Append vh/vv to existing image (vv, vh) channels
             norm_images = [torch.cat((image, (image[:, 1:2] / (image[:, 0:1] + EPS))), dim=1) for image in norm_images]
+
+        if self.requires_dem or self.requires_slope:
+            if self.requires_slope:
+                dem = _calculate_slope(dem, dem_nodata)
+            # Append DEM to existing image channels
+            dem = einops.rearrange(dem, "b h w -> b 1 h w")
+            norm_images = [torch.cat((image, dem), dim=1) for image in norm_images]
 
         # Input concat on channels, or as separate inputs
         if self.cat_input:
@@ -90,5 +133,14 @@ def snunet(pretrained=False):
         release_url = "https://github.com/Multihuntr/KuroSiwo/releases/download/v0.0.1-alpha/kurosiwo_snunet_weights.pt"
         model.load_state_dict(torch.hub.load_state_dict_from_url(release_url))
 
-    wrapped = PreprocessingWrapper(model, IMG_MEANS, IMG_STDS, append_ratio=True, cat_input=False)
+    wrapped = PreprocessingWrapper(
+        model,
+        IMG_MEANS,
+        IMG_STDS,
+        append_ratio=False,
+        cat_input=False,
+        requires_slope=True,
+        dem_mean=DEM_MEAN,
+        dem_std=DEM_STD,
+    )
     return wrapped.eval()

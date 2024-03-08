43ec8cf97b659b0de92ff3afc61f2e5ff4d5e41b
Brandon Victor <multihuntr@hotmail.com>
2024-03-08 12:57:09 +0000

    Fixed image order issue. Fixed DEM processing.

--- hubconf.py
+++ hubconf.py
@@ -17,6 +17,8 @@ IMG_MEANS = [0.0953, 0.0264]
 IMG_STDS = [0.0427, 0.0215]
 DEM_MEAN = 67.0293
 DEM_STD = 1765.0062
+SLOPE_MEAN = 2.9482
+SLOPE_STD = 79.2493
 EPS = 1e-7
 
 
@@ -24,12 +26,11 @@ def _calculate_slope(dem, nodata):
     """
     Calculates the slope of the DEM. DEM should be shaped [b h w]
     """
-    res = torch.empty_like(dem)
-    dem_np = dem.numpy()
+    res = np.empty_like(dem)
     for i in range(dem.shape[0]):
-        dem_i = rd.rdarray(dem_np[i], no_data=nodata)
+        dem_i = rd.rdarray(dem[i], no_data=nodata)
         dem_i = rd.TerrainAttribute(dem_i, attrib="slope_riserun")
-        res[i] = torch.tensor(np.asarray(dem_i.data))
+        res[i] = np.asarray(dem_i.data)
     return res
 
 
@@ -40,7 +41,9 @@ class PreprocessingWrapper(nn.Module):
     reshape as necessary, and provides a consistent interface across models.
 
     The list can be either 2 or 3 elements long, representing the timesteps:
-        [pre1] pre2 post
+        [pre2] pre1 post
+
+    If providing DEM, it must be provided as a numpy array.
     """
 
     def __init__(
@@ -50,10 +53,13 @@ class PreprocessingWrapper(nn.Module):
         stds,
         append_ratio=False,
         cat_input=True,
+        img_order_forward=True,
         requires_dem=False,
         requires_slope=False,
         dem_mean=None,
         dem_std=None,
+        slope_mean=None,
+        slope_std=None,
     ):
         super().__init__()
         self.model = model
@@ -61,14 +67,18 @@ class PreprocessingWrapper(nn.Module):
         self.stds = stds
         self.append_ratio = append_ratio
         self.cat_input = cat_input
+        self.img_order_forward = img_order_forward
         self.requires_dem = requires_dem
         self.requires_slope = requires_slope
         self.dem_mean = dem_mean
         self.dem_std = dem_std
         self.transform = torchvision.transforms.Normalize(means, stds)
-        if (requires_dem or requires_slope) and (dem_mean is None or dem_std is None):
+        if (requires_dem) and (dem_mean is None or dem_std is None):
             raise Exception("Using a DEM requires normalisation parameters.")
         self.dem_transform = torchvision.transforms.Normalize(dem_mean, dem_std)
+        if (requires_slope) and (slope_mean is None or slope_std is None):
+            raise Exception("Using a slope requires normalisation parameters.")
+        self.slope_transform = torchvision.transforms.Normalize(slope_mean, slope_std)
 
     def forward(self, images, dem=None, dem_nodata=np.nan):
         if (self.requires_dem or self.requires_slope) and dem is None:
@@ -82,11 +92,21 @@ class PreprocessingWrapper(nn.Module):
             norm_images = [torch.cat((image, (image[:, 1:2] / (image[:, 0:1] + EPS))), dim=1) for image in norm_images]
 
         if self.requires_dem or self.requires_slope:
+            dtype = norm_images[0].dtype
+            device = norm_images[0].device
             if self.requires_slope:
-                dem = _calculate_slope(dem, dem_nodata)
+                slope = _calculate_slope(dem, dem_nodata)
+                slope = torch.tensor(slope, dtype=dtype, device=device)
+                norm_dem = self.slope_transform(slope)
+            else:
+                dem = torch.tensor(dem, dtype=dtype, device=device)
+                norm_dem = self.dem_transform(dem)
             # Append DEM to existing image channels
-            dem = einops.rearrange(dem, "b h w -> b 1 h w")
-            norm_images = [torch.cat((image, dem), dim=1) for image in norm_images]
+            norm_dem = einops.rearrange(norm_dem, "b h w -> b 1 h w")
+            norm_images = [torch.cat((image, norm_dem), dim=1) for image in norm_images]
+
+        if not self.img_order_forward:
+            norm_images = list(reversed(norm_images))
 
         # Input concat on channels, or as separate inputs
         if self.cat_input:
@@ -113,23 +133,21 @@ def vit_decoder(pretrained=False):
         "decoder": True,
     }
     model = FinetunerSegmentation(encoder=vit, configs=configs)
-    model.input_hint = "Input is 3x Image(VV, VH) as (pre1, pre2, post)"
+    model.input_hint = "Input is 3x Image(VV, VH) as (pre2, pre1, post)"
     if pretrained:
-        # TODO: Replace with real release URL
         release_url = (
             "https://github.com/Multihuntr/KuroSiwo/releases/download/v0.0.1-alpha/kurosiwo_vit_decoder_weights.pt"
         )
         model.load_state_dict(torch.hub.load_state_dict_from_url(release_url))
 
-    wrapped = PreprocessingWrapper(model, IMG_MEANS, IMG_STDS, append_ratio=False, cat_input=True)
+    wrapped = PreprocessingWrapper(model, IMG_MEANS, IMG_STDS, append_ratio=False, cat_input=True, img_order_forward=False)
     return wrapped.eval()
 
 
 def snunet(pretrained=False):
     model = SNUNet_ECAM(3, 3, 32)
-    model.input_hint = "Input is 2x Image(VV, VH) as (pre1, post)"
+    model.input_hint = "Input is 2x Image(VV, VH) as (pre1, post) and dem as a np.ndarray"
     if pretrained:
-        # TODO: Replace with real release URL
         release_url = "https://github.com/Multihuntr/KuroSiwo/releases/download/v0.0.1-alpha/kurosiwo_snunet_weights.pt"
         model.load_state_dict(torch.hub.load_state_dict_from_url(release_url))
 
@@ -140,7 +158,7 @@ def snunet(pretrained=False):
         append_ratio=False,
         cat_input=False,
         requires_slope=True,
-        dem_mean=DEM_MEAN,
-        dem_std=DEM_STD,
+        slope_mean=SLOPE_MEAN,
+        slope_std=SLOPE_STD,
     )
     return wrapped.eval()

d6f8a8429e7c41ecfa094dc16e86012a25c1fc09
ngbountos <mpountos@outlook.com.gr>
2024-07-13 21:07:26 +0300

    update GRD downloading script

--- download_kuro_siwo.sh
+++ download_kuro_siwo.sh
@@ -10,16 +10,20 @@ full_path="$root_folder_path/KuroSiwo"
 
 mkdir $full_path
 
-cat_url="https://www.dropbox.com/scl/fi/vl9iea5k9fvq35oaybsy2/catalogue.gpkg?rlkey=5ekxjfj9ynor7gk9rom7st5kd&dl=0"
-batch_1_url="https://www.dropbox.com/scl/fi/vcmzontrqlywkb6827xr4/01.tar.gz?rlkey=p08coyujc8jtqc9omp8pee4u9&dl=0"
-batch_2_url="https://www.dropbox.com/scl/fi/9eiskq6bvwvtkcgar7fau/02.tar.gz?rlkey=jjd8uitqa31wmivcirg84q83o&dl=0"
-batch_3_url="https://www.dropbox.com/scl/fi/wal95mj2lgrhn5vfv82ru/03.tar.gz?rlkey=fxgfccf16v8bmawhkrlivat3s&dl=0"
-batch_4_url="https://www.dropbox.com/scl/fi/r6zfyfkauctuvdgondul0/04.tar.gz?rlkey=zq4rnjljvyph7udidfntgn5p1&dl=0"
-batch_5_url="https://www.dropbox.com/scl/fi/rwvumfstxp27k1akv0hv5/05.tar.gz?rlkey=ukysm10tljygxourdkbwzkr2k&dl=0"
-batch_6_url="https://www.dropbox.com/scl/fi/uqobj3knewqm5y6a9cax3/06.tar.gz?rlkey=oub1c8905gwuthjlvz6lydib1&dl=0"
-batch_7_url="https://www.dropbox.com/scl/fi/7uamc6g165so3mfv9n411/07.tar.gz?rlkey=53x6tmmmh7iqbbzizu64vonco&dl=0"
-batch_8_url="https://www.dropbox.com/scl/fi/ejztwj7ib545qsk7kw8ou/08.tar.gz?rlkey=tbl0bk3kzqe77ugs5fucdpkd4&dl=0"
-urls=($cat_url $batch_1_url $batch_2_url $batch_3_url $batch_4_url $batch_5_url $batch_6_url $batch_7_url $batch_8_url)
+cat_url="https://www.dropbox.com/scl/fi/wu6nvj73cz4h7k3gxpzx6/catalogue.gpkg?rlkey=hsij2o0k60r2n0z6z4d2ngww9&st=0zjqhzgx&dl=0"
+batch_1_url="https://www.dropbox.com/scl/fi/19mm9v5pnd5yor8b15alj/00.tar.gz?rlkey=f7qrqgv7h7z9j6r595xz1720e&st=k4ptwanm&dl=0"
+batch_2_url="https://www.dropbox.com/scl/fi/pjlgcqc3fm8lx97vdfjyn/01.tar.gz?rlkey=bvue0u3jgovc3qewkh464uyog&st=qfsa601d&dl=0"
+batch_3_url="https://www.dropbox.com/scl/fi/7tt843025s8hqi00xpofx/02.tar.gz?rlkey=8vbfh3qc2h7zmzjem0c14lljh&st=xda2zf5j&dl=0"
+batch_4_url="https://www.dropbox.com/scl/fi/v7997kv11cc8ptj1vstim/03.tar.gz?rlkey=yj0qspgmlmohaf728a94curqf&st=6qmn63y7&dl=0"
+batch_5_url="https://www.dropbox.com/scl/fi/2o3cxwxu79phijxylm99h/04.tar.gz?rlkey=3mcmjrzbf8vmxwl8aacou7vvr&st=yh0e4p72&dl=0"
+batch_6_url="https://www.dropbox.com/scl/fi/q1jy4ep4j6bj38dva2es3/05.tar.gz?rlkey=g46498pgox03it080p83xhx8g&st=haxheepl&dl=0"
+batch_7_url="https://www.dropbox.com/scl/fi/843fba3poe6nu67og5n6x/06.tar.gz?rlkey=bnxhf9zj0y8mtvjv7k683ywaa&st=p4pg93ko&dl=0"
+batch_8_url="https://www.dropbox.com/scl/fi/89kogi7nisinbfa3z2wze/07.tar.gz?rlkey=byduc9q5cndwjphq4r1727xrl&st=a2sgt9dp&dl=0"
+batch_9_url="https://www.dropbox.com/scl/fi/sctg1ybqxtzz23018htwx/08.tar.gz?rlkey=6hj887jxezjq5gibqz4zhh7gd&st=9gcgiwdu&dl=0"
+batch_10_url="https://www.dropbox.com/scl/fi/425wrfqqc7sy8pbdnkgzw/09.tar.gz?rlkey=ziejzpo2ddgnygpbd1emn1nir&st=irjm2j98&dl=0"
+batch_11_url="https://www.dropbox.com/scl/fi/rwc2yvd0g070qicg300de/10.tar.gz?rlkey=12i17si2kzjpey4vgig9pjnv7&st=p2v45gue&dl=0"
+
+urls=($cat_url $batch_1_url $batch_2_url $batch_3_url $batch_4_url $batch_5_url $batch_6_url $batch_7_url $batch_8_url $batch_9_url $batch_10_url)
 
 downloaded_files=()
 failed_downloads=()
